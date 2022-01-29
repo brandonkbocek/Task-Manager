d4c850862637fe93f241fccaf6879c0b0c1a7c7b
brandonkbocek <brandon.bocek@gmail.com>
2022-01-28 20:53:00 -0600

    async user apis

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,32 +1,13 @@
 const mongoose = require('mongoose')
 
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    // useNewUrlParser: true,
+    // useNewUrlParser: true
     // useCreateIndex: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-})
 
-const task = new Task({
-    description: 'Clean room',
-    completed: false
-})
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+
+
