8ba3a540ebad902a3abc544f405bbb36e7af5562
brandonkbocek <brandon.bocek@gmail.com>
2022-01-31 19:05:02 -0600

    url options

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
+const { query } = require('express')
 const router = new express.Router()
 
 
@@ -19,11 +20,33 @@ router.post('/tasks', auth, async(req, res) => {
     }
 })
 
+// GET /tasks?completed=false
+// GET /tasks?limit=10&skip=0
+// GET /tass?sortBy=createdAt:asc
 router.get('/tasks', auth, async(req, res) => {
-    
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try{
-        const tasks = await Task.find({ owner: req.user._id })
-        res.send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        })
+        res.send(req.user.tasks)
     }catch(e){
         res.send(500).send()
     }
