5961fe3a859def61f19f3dec3cfe0851aca06253
brandonkbocek <brandon.bocek@gmail.com>
2022-02-01 19:52:43 -0600

    email sends

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const User = require('./models/user')
 const Task = require('./models/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 
